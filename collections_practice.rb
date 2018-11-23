ba0113ac563888bae79abe4512a6e83b71b2cc71
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 12:30:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,10 +11,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  temp = array[2]
-  array[2] = array[1]
-  array[1] = temp
-  return array
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
