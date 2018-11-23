a0112944c0a392235deec8f547e58c8d361bb543
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 12:31:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,12 +20,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  new_array = []
-  array.each {|word|
-    word[2] = "$";
-    new_array << word}
-  puts new_array.inspect
-  new_array
+  array.each {|word| word[2] = "$"}
 end
 
 def find_a?(array)
